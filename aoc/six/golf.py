2ed0e10400b2c96ad805472937f045e805879d0c
Frode Jacobsen <frode@aidn.no>
2023-12-06 22:27:04 +0100

    day six (golf): shave off 3 bytes

--- aoc/six/golf.py
+++ aoc/six/golf.py
@@ -1,4 +1,4 @@
 s,a=lambda t,r:int((t**2-4*r)**.5)+1,1
-t=zip(*[map(int,[''.join(k)]+k)for _,*k in map(str.split,open(0))]);b=s(*next(t))
+b,*t=zip(*[map(int,[''.join(k)]+k)for _,*k in map(str.split,open(0))]);b=s(*b)
 for p in t:a*=s(*p)
 print(a,b)
