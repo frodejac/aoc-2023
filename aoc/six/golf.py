85b7064776d0d05bcfeab33b9d0c98d32477a58e
Frode Jacobsen <frode@aidn.no>
2023-12-06 23:45:54 +0100

    day six (golf): one more byte -> 147B !noforward
    though strictly not correct for integer roots (e.g. input (8, 12) should be 3 but this return 5)
    correct 1st line is: s,a=lambda t,r:sum(r<h*(t-h)for h in range(t)),1
    the correct line is 11 bytes longer + brute force

--- aoc/six/golf.py
+++ aoc/six/golf.py
@@ -1,4 +1,4 @@
-s,a=lambda t,r:int((t**2-4*r)**.5)+1,1
+s,a=lambda t,r:int((t*t-4*r)**.5)+1,1
 b,*t=zip(*[map(int,[''.join(k)]+k)for _,*k in map(str.split,open(0))]);b=s(*b)
 for p in t:a*=s(*p)
 print(a,b)
