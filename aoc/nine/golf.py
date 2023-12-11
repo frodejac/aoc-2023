0ef1712305951897f9c0c1dd09e3afbe00198a3c
Frode Jacobsen <frode@aidn.no>
2023-12-11 21:38:55 +0100

    day nine golf: 19 bytes shorter using recursion

--- aoc/nine/golf.py
+++ aoc/nine/golf.py
@@ -1,7 +1,3 @@
-a=b=0
-for*s,in[map(int,l.split())for l in open(0)]:
- k=[s];g=h=0
- while any(k[0]):k=[[j-i for i,j in zip(k[0],k[0][1:])]]+k
- for p in k:g+=p[-1];h=p[0]-h
- a+=g;b+=h
+a=b=0;m=lambda s,f:any(s)and s[-f]+(f*2-1)*m([j-i for i,j in zip(s,s[1:])],f)or 0
+for*s,in[map(int,l.split())for l in open(0)]:a+=m(s,1);b+=m(s,0)
 print(a,b)
